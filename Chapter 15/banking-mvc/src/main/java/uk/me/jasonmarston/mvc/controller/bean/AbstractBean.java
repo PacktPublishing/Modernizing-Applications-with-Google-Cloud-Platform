a24d4b1991f61eead01dd88dee92e540bcef3991
shruthis-shetty <76615772+shruthis-shetty@users.noreply.github.com>
2021-11-06 10:58:51 +0530

    Add files via upload

---
+++ Chapter 15/banking-mvc/src/main/java/uk/me/jasonmarston/mvc/controller/bean/AbstractBean.java
@@ -0,0 +1,24 @@
+package uk.me.jasonmarston.mvc.controller.bean;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public abstract class AbstractBean {
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, 
+				ToStringStyle.MULTI_LINE_STYLE);
+	}
+}
