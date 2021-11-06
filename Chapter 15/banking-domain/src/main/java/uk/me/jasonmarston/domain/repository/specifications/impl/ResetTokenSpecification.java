30640fdc8f519457e6f7ea96b78ca739474ba02f
shruthis-shetty <76615772+shruthis-shetty@users.noreply.github.com>
2021-11-06 10:24:59 +0530

    Add files via upload

---
+++ Chapter 15/banking-domain/src/main/java/uk/me/jasonmarston/domain/repository/specifications/impl/ResetTokenSpecification.java
@@ -0,0 +1,35 @@
+package uk.me.jasonmarston.domain.repository.specification.impl;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import uk.me.jasonmarston.domain.aggregate.impl.ResetToken;
+
+public class ResetTokenSpecification {
+	public static class ResetTokenIsExpired 
+			implements Specification<ResetToken> {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public Predicate toPredicate(
+				final Root<ResetToken> root, 
+				final CriteriaQuery<?> query, 
+				final CriteriaBuilder builder) {
+			final Instant now = Instant.now();
+			final ZoneId utc = ZoneId.of("UTC");
+			final ZonedDateTime current = ZonedDateTime.ofInstant(now, utc);
+
+			return builder.lessThanOrEqualTo(
+					root.<ZonedDateTime>get("expiryDate"),
+					current);
+		}
+	}
+}
