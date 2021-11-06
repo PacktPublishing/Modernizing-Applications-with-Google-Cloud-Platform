4b7d8204814bb419ae2315c6e0271b67289fafe9
shruthis-shetty <76615772+shruthis-shetty@users.noreply.github.com>
2021-11-06 10:19:21 +0530

    Add files via upload

---
+++ Chapter 15/banking-domain/src/main/java/uk/me/jasonmarston/domain/factory/aggregate/VerificationTokenBuilderFactory.java
@@ -0,0 +1,7 @@
+package uk.me.jasonmarston.domain.factory.aggregate;
+
+import uk.me.jasonmarston.domain.aggregate.impl.VerificationToken;
+import uk.me.jasonmarston.framework.domain.factory.IFactory;
+
+public interface VerificationTokenBuilderFactory extends IFactory<VerificationToken.Builder> {
+}
