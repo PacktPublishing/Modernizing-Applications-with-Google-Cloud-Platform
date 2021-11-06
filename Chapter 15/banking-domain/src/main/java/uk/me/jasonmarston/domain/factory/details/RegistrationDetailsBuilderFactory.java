6bef1a7521f72aa7053a403b28cfd3c6777a446b
shruthis-shetty <76615772+shruthis-shetty@users.noreply.github.com>
2021-11-06 10:20:35 +0530

    Add files via upload

---
+++ Chapter 15/banking-domain/src/main/java/uk/me/jasonmarston/domain/factory/details/RegistrationDetailsBuilderFactory.java
@@ -0,0 +1,7 @@
+package uk.me.jasonmarston.domain.factory.details;
+
+import uk.me.jasonmarston.domain.details.impl.RegistrationDetails;
+import uk.me.jasonmarston.framework.domain.factory.IFactory;
+
+public interface RegistrationDetailsBuilderFactory extends IFactory<RegistrationDetails.Builder> {
+}
