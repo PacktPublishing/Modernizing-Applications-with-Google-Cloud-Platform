b26d30c606a33347a3192c68de420aeb13c0f1d6
shruthis-shetty <76615772+shruthis-shetty@users.noreply.github.com>
2021-11-08 12:11:52 +0530

    Add files via upload

---
+++ Chapter 13/account-domain/src/main/java/uk/me/jasonmarston/domain/account/factory/details/TransferIdentifierDetailsBuilderFactory.java
@@ -0,0 +1,7 @@
+package uk.me.jasonmarston.domain.account.factory.details;
+
+import uk.me.jasonmarston.domain.account.details.impl.TransferIdentifierDetails;
+import uk.me.jasonmarston.framework.domain.factory.IFactory;
+
+public interface TransferIdentifierDetailsBuilderFactory extends IFactory<TransferIdentifierDetails.Builder> {
+}
