9a9b2a16036ac7bc1635c776434b594aca26fd66
shruthis-shetty <76615772+shruthis-shetty@users.noreply.github.com>
2021-11-08 15:55:23 +0530

    Add files via upload

---
+++ Chapter 13/user-core-domain/src/main/java/uk/me/jasonmarston/domain/user/Constants.java
@@ -0,0 +1,8 @@
+package uk.me.jasonmarston.domain.user;
+
+public class Constants {
+	public static final String STRONG_PASSWORD = "^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!'\"@#\\$%\\^&\\*\\(\\)\\-=_\\+\\[\\]\\{\\},.\\/\\|<>\\?`¬¦]).{8,})";
+
+	private Constants() {
+	}
+}
